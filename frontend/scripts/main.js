ce5c8a393b2d05c1c7f1880f819189eb619c853a
Angga Bawole <bawoleangga03@gmail.com>
2021-04-05 01:20:18 +0800

    rating modal

---
+++ frontend/scripts/main.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($){
+	    
+	$(".btnrating").on('click',(function(e) {
+	
+	var previous_value = $("#selected_rating").val();
+	
+	var selected_value = $(this).attr("data-attr");
+	$("#selected_rating").val(selected_value);
+	
+	$(".selected-rating").empty();
+	$(".selected-rating").html(selected_value);
+	
+	for (i = 1; i <= selected_value; ++i) {
+	$("#rating-star-"+i).toggleClass('btn-outline-warning');
+	$("#rating-star-"+i).toggleClass('btn-default');
+	}
+	
+	for (ix = 1; ix <= previous_value; ++ix) {
+	$("#rating-star-"+ix).toggleClass('btn-outline-warning');
+	$("#rating-star-"+ix).toggleClass('btn-default');
+	}
+	
+	}));
+	
+		
+});
